0128f68c8a69fc6a5e7b835e5bcf4304eb16ebdb
Diego F. Goberna <github@feiss.be>
2023-08-16 19:41:57 +0200

    zoom & pan

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ class App {
     constructor(config) {
         this.W = config.width;
         this.H = config.width * config.page_ratio;
-        this.line_height = 100;
+        this.line_height = 60;
         this.grid_ratio = config.grid_ratio;
 
         this.pages = [];
@@ -26,10 +26,11 @@ class App {
         this.canvas = $('#canvas');
 
         this.zoom = this.W * 1.06 / window.innerWidth;
+        // this.zoom = 0.7;
+
         this.pan = new Victor(20, 20);
 
         this.is_mouse = true;
-        this.action = ACTION_NONE;
 
         this.events = new EventHandler(this);
     }
@@ -75,6 +76,16 @@ class App {
         render();
     }
 
+    do_zoom(dir) {
+        this.zoom += dir * 0.1;
+        this.zoom = Math.max(0.2, Math.min(5, this.zoom));
+        this.render();
+    }
+
+    do_pan(dir) {
+        this.pan = this.pan.add(dir);
+        this.render();
+    }
 }
 
 
--- events.js
+++ events.js
@@ -2,37 +2,43 @@
 const ACTION_NONE = 'NONE';
 const ACTION_DRAWING = 'DRAWING';
 const ACTION_PANNING = 'PANNING';
-const MIN_MOVEMENT = 8;
 
 class EventHandler {
     prev;
 
     constructor(app) {
         this.recognizer = new Recognizer();
+        this.action = ACTION_NONE;
 
         app.canvas.addEventListener('pointerdown', this.on_pointer.bind(this));
         app.canvas.addEventListener('pointermove', this.on_pointer.bind(this));
         app.canvas.addEventListener('pointerup', this.on_pointer.bind(this));
+        app.canvas.addEventListener('wheel', this.on_wheel.bind(this));
+        app.canvas.addEventListener('contextmenu', this.on_context.bind(this));
         app.canvas.addEventListener('pointercancel', this.on_pointer.bind(this));
     }
 
     on_pointer(ev) {
         if (ev.target.tagName == 'A') return;
-        // var rect = app.canvas.getBoundingClientRect();
-        let x = (ev.clientX - app.pan.x) * app.zoom;
-        let y = (ev.clientY - app.pan.y) * app.zoom;
+
+        if (ev.button == 2 || this.action == ACTION_PANNING) {
+            return this.on_alt_pointer(ev);
+        }
+        // TODO: remove scrollleft and use only app.pan
+        let x = (ev.clientX - app.pan.x + $('#canvas').scrollLeft) * app.zoom;
+        let y = (ev.clientY - app.pan.y + $('#canvas').scrollTop) * app.zoom;
         switch (ev.type) {
             case 'pointerdown':
                 app.is_mouse = ev.pointerType == "mouse";
                 this.recognizer.restart();
-                app.action = ACTION_DRAWING;
-
-                const shape = app.page.add_stroke(10, '#215');
+                this.action = ACTION_DRAWING;
+                const shape = app.page.add_stroke(5, '#215');
                 shape.add_point(x, y);
                 this.prev = new Victor(x, y);
                 break;
             case 'pointermove':
-                if (app.action != ACTION_DRAWING) return;
+                if (this.action != ACTION_DRAWING) return;
+                const MIN_MOVEMENT = 8 * app.zoom;
                 if (Math.abs(x - this.prev.x) > MIN_MOVEMENT || Math.abs(y - this.prev.y) > MIN_MOVEMENT) {
                     app.page.active_shape.add_point(x, y);
                     // if (DEBUG) c.strokeRect(x - 1, y - 1, 3, 3)
@@ -40,16 +46,50 @@ class EventHandler {
                 }
                 break;
             case 'pointerup':
-                if (app.action == ACTION_DRAWING) {
+                if (this.action == ACTION_DRAWING) {
                     app.page.active_shape.add_point(x, y);
-                    app.action = ACTION_NONE;
+                    this.action = ACTION_NONE;
                     this.recognizer.recognize(app.page.active_shape);
                 }
                 break;
             case 'pointercancel':
-                app.action = ACTION_NONE;
+                this.action = ACTION_NONE;
                 this.recognizer.recognize(app.page.active_shape);
                 break;
+        }
+    }
+
+    on_wheel(ev) {
+        ev.preventDefault();
+        let delta = ev.deltaY;
+        if (delta == 0) return;
+        delta = ev.deltaY > 0 ? 1 : -1;
+        app.do_zoom(delta);
+    }
+
+    on_context(ev) {
+        ev.preventDefault();
+    }
+
+    on_alt_pointer(ev) {
+        this.action = ACTION_PANNING;
+        switch (ev.type) {
+            case 'pointerdown':
+                this.action = ACTION_PANNING;
+                this.prev = new Victor(ev.clientX, ev.clientY);
+                break;
+            case 'pointermove':
+                let pan = new Victor(ev.clientX, ev.clientY).subtract(this.prev);
+                app.do_pan(pan);
+                this.prev = new Victor(ev.clientX, ev.clientY);
+                break;
+            case 'pointerup':
+                this.action = ACTION_NONE;
+                break;
+            case 'pointercancel':
+                this.action = ACTION_NONE;
+                break;
+
         }
     }
 }
