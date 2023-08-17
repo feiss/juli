2a3307f46b563c73787b942cd1f9ffac57eefd81
Diego F. Goberna <github@feiss.be>
2023-08-17 23:16:09 +0200

    recognize simple

--- recognizer.js
+++ recognizer.js
@@ -2,12 +2,13 @@
 class Recognizer {
     waiting_next_stroke;
     shape;
-    stroke;
+    coords;
     bb;
     prev_shape;
     prev_bb;
     prev_angles;
     prev_result;
+    prev_cell;
 
     constructor() {
         this.bb = {
@@ -35,45 +36,43 @@ class Recognizer {
 
 
     recognize(shape) {
+        let result;
         this.shape = shape;
-        this.stroke = [];
+        this.coords = [];
         let candidates = this.#init_candidates();
 
 
         for (let i = 0; i < shape.coords.length; i++) {
-            this.stroke.push({ x: shape.coords[i].x, y: shape.coords[i].y });
+            this.coords.push({ x: shape.coords[i].x, y: shape.coords[i].y });
         }
-        let stroke = this.stroke;
+        let coords = this.coords;
+
+        let simple = this.#recognize_simple();
+
+        if (simple) {
+            //
+        }
 
         if (!this.waiting_next_stroke) {
             this.bb = structuredClone(shape.bounds);
         }
 
-        if (shape.coords.length == 2 && shape.coords[0].distance(shape.coords[1]) < app.line_height * 0.1) {
-            stroke = [{ x: shape.coords[0].x, y: shape.coords[0].y }];
+
+        if (simple) {
+            result = [simple];
         } else {
             this.#normalize();
             this.#simplify_by_angle(0.8);
             this.#simplify_by_angle(0.4);
             this.#simplify_by_distance(0.1);
             this.#simplify_by_distance(0.2);
-        }
 
-        let angles = [];
-        for (let i = 0; i < stroke.length - 1; i++) {
-            let segment = new Victor(stroke[i + 1].x - stroke[i].x, -(stroke[i + 1].y - stroke[i].y));
-            let angle = segment.angle() < 0 ? segment.angle() + Math.PI * 2 : segment.angle();
-            angles.push(direction(angle));
-        }
-        let result;
-
-        if (angles.length == 0) {
-            // dot
-            // let x = this.bb.x % (app.line_height * app.grid_ratio);
-            let y = this.bb.y % app.line_height;
-            if (y > app.line_height * 0.66) result = ['.'];
-            else result = ['·'];
-        } else {
+            let angles = [];
+            for (let i = 0; i < coords.length - 1; i++) {
+                let segment = new Victor(coords[i + 1].x - coords[i].x, -(coords[i + 1].y - coords[i].y));
+                let angle = segment.angle() < 0 ? segment.angle() + Math.PI * 2 : segment.angle();
+                angles.push(direction(angle));
+            }
 
             // find candidates
             for (let i = 0; i < angles.length; i++) {
@@ -93,26 +92,34 @@ class Recognizer {
             // console.log(recognized);
             // get set of uniq values with digits
             result = [...new Set(recognized.map(v => v.digit))];
-        }
 
-        // decide between 0 and 6
-        if (result.indexOf(0) !== -1 && result.indexOf(6) !== -1) {
-            const diff = stroke[stroke.length - 1].y - stroke[0].y;
-            if (diff > 0.2) {
-                result = [6];
-            } else {
-                result = [0];
+            // decide between 0 and 6
+            if (result.indexOf(0) !== -1 && result.indexOf(6) !== -1) {
+                const diff = coords[coords.length - 1].y - coords[0].y;
+                if (diff > 0.2) {
+                    result = [6];
+                } else {
+                    result = [0];
+                }
+            }
+
+            this.prev_angles = [...angles];
+            if (navigator['clipboard']) {
+                navigator.clipboard.writeText(angles.join(', '));
             }
         }
 
 
         if (this.waiting_next_stroke) {
+            const a = this.#cell_from_point(coords[0]);
+            const same_cells = a.x == this.prev_cell.x && a.y == this.prev_cell.y;
+
             if (this.prev_result[0] == 7) {
                 if (result[0] == '7b') {
-                    const lastpoint = stroke[stroke.length - 1];
-                    const prevpoint = stroke[stroke.length - 2];
+                    const lastpoint = coords[coords.length - 1];
+                    const prevpoint = coords[coords.length - 2];
                     if (prev_bb.y == bb.y && prev_bb.y2 == bb.y2) {
-                        bb = prev_bb;
+                        this.bb = this.prev_bb;
                         result = [7];
                     } else {
                         result = [];
@@ -120,8 +127,7 @@ class Recognizer {
                 } else {
                     result = [];
                 }
-            } else if (this.prev_result[0] == '·') {
-                console.log('prev was ·, now is', result[0]);
+            } else if (same_cells && this.prev_result[0] == '·') {
                 if (result[0] == '.') {
                     result[0] = ':';
                 }
@@ -133,17 +139,12 @@ class Recognizer {
             }
         }
 
-
         this.prev_result = [...result];
-        this.prev_angles = [...angles];
-
-        if (navigator['clipboard']) {
-            navigator.clipboard.writeText(angles.join(', '));
-        }
-
 
         if (!this.waiting_next_stroke && result.length > 0 && (result[0] == 7 || result[0] == '·')) {
             this.waiting_next_stroke = true;
+            this.prev_cell = this.#cell_from_point(coords[0]);
+
             setTimeout(this.#cancel_wait.bind(this), 700);
         } else {
             this.waiting_next_stroke = false;
@@ -155,15 +156,22 @@ class Recognizer {
 
     #show_result() {
         if (this.prev_shape) this.prev_shape.el.parentNode.removeChild(this.prev_shape.el);
-        this.shape.el.parentNode.removeChild(this.shape.el);
+        if (this.shape) this.shape.el.parentNode.removeChild(this.shape.el);
 
-        this.shape = null;
-        this.prev_shape = null;
+        // this.shape = null;
+        // this.prev_shape = null;
+
+        const cell_width = app.line_height * app.grid_ratio;
+        const cell_height = app.line_height;
 
         let x = (this.bb.x + this.bb.x2) / 2;
         let y = (this.bb.y + this.bb.y2) / 2;
-        x = Math.floor(x / (app.line_height * app.grid_ratio)) * app.line_height * app.grid_ratio;
-        y = Math.floor(y / app.line_height) * app.line_height;
+
+        let cell = this.#cell_from_point(new Victor(x, y));
+        this.prev_cell = cell.clone();
+
+        x = cell.x * cell_width;
+        y = cell.y * cell_height;
 
         if (this.prev_result.length > 0) {
             console.log('result: ', this.prev_result[0]);
@@ -186,30 +194,66 @@ class Recognizer {
     }
 
 
+    #recognize_simple() {
+        let coords = this.shape.coords;
+
+        // dot
+        let accum_distances = 0;
+        const center = coords[0];
+        for (let i = 1; i < coords.length; i++) {
+            accum_distances += coords[i - 1].distance(coords[i]);
+        }
+        if (accum_distances < app.line_height * 0.3) {
+            let p = this.#in_cell_coords(center);
+
+            if (p.y > 0.66) return '.';
+            else return '·';
+        }
+        return null;
+    }
+
+    #cell_from_point(p) {
+        const cell_width = app.line_height * app.grid_ratio;
+        const cell_height = app.line_height;
+        return new Victor(
+            Math.floor(p.x / cell_width),
+            Math.floor(p.y / cell_height)
+        );
+    }
+
+    #in_cell_coords(p) {
+        const cell_width = app.line_height * app.grid_ratio;
+        const cell_height = app.line_height;
+        return new Victor(
+            p.x % cell_width / cell_width,
+            p.y % cell_height / cell_height
+        );
+    }
+
     #normalize() {
         let w = this.bb.x2 - this.bb.x;
         let h = this.bb.y2 - this.bb.y;
         // let size = Math.max(w, h);
 
-        for (let i = 0; i < this.stroke.length; i++) {
-            this.stroke[i].x = (this.stroke[i].x - this.bb.x) / w;
-            this.stroke[i].y = (this.stroke[i].y - this.bb.y) / h;
+        for (let i = 0; i < this.coords.length; i++) {
+            this.coords[i].x = (this.coords[i].x - this.bb.x) / w;
+            this.coords[i].y = (this.coords[i].y - this.bb.y) / h;
         }
     }
 
     #simplify_by_distance(threshold) {
         while (true) {
-            let prev_length = this.stroke.length;
-            for (let i = 1; i < this.stroke.length; i++) {
-                let a = Victor.fromObject(this.stroke[i - 1]);
-                let b = Victor.fromObject(this.stroke[i]);
+            let prev_length = this.coords.length;
+            for (let i = 1; i < this.coords.length; i++) {
+                let a = Victor.fromObject(this.coords[i - 1]);
+                let b = Victor.fromObject(this.coords[i]);
 
                 if (a.distance(b) < threshold) {
-                    this.stroke.splice(i, 1);
+                    this.coords.splice(i, 1);
                     break;
                 }
             }
-            if (prev_length == this.stroke.length) {
+            if (prev_length == this.coords.length) {
                 break;
             }
         }
@@ -217,15 +261,15 @@ class Recognizer {
 
     #simplify_by_angle(threshold) {
         while (true) {
-            let prev_length = this.stroke.length;
+            let prev_length = this.coords.length;
             let sorted = [];
-            if (this.stroke.length < 3) {
+            if (this.coords.length < 3) {
                 break;
             }
-            for (let i = 1; i < this.stroke.length - 1; i++) {
-                let a = Victor.fromObject(this.stroke[i - 1]);
-                let b = Victor.fromObject(this.stroke[i]);
-                let c = Victor.fromObject(this.stroke[i + 1]);
+            for (let i = 1; i < this.coords.length - 1; i++) {
+                let a = Victor.fromObject(this.coords[i - 1]);
+                let b = Victor.fromObject(this.coords[i]);
+                let c = Victor.fromObject(this.coords[i + 1]);
 
                 let B = c.subtract(b);
                 let A = b.subtract(a);
@@ -236,9 +280,9 @@ class Recognizer {
             }
             sorted.sort((a, b) => b.dot - a.dot);
             if (sorted[0].dot > threshold) {
-                this.stroke.splice(sorted[0].index, 1);
+                this.coords.splice(sorted[0].index, 1);
             }
-            if (prev_length == this.stroke.length || this.stroke.length < 3) {
+            if (prev_length == this.coords.length || this.coords.length < 3) {
                 break;
             }
         }
